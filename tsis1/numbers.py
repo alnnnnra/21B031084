7cf2400fdeee82f27a8195aa8f67bccc90d2a17e
alnnnnra <123537239+alnnnnra@users.noreply.github.com>
2023-02-08 16:42:58 +0600

    Add files via upload

---
+++ tsis1/numbers.py
@@ -0,0 +1,63 @@
+import random
+# ex24
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+# ex25
+print(type(x))
+print(type(y))
+print(type(z))
+# ex26
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+# ex27
+x = 1.10
+y = 1.0
+z = -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+# ex28
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+# ex29
+x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
+# ex30
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+# convert from int to float:
+a = float(x)
+
+# convert from float to int:
+b = int(y)
+
+# convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+# ex31
+print(random.randrange(1, 10))
+# ex32
+x = 5
+x = float(x)
